051cd3039213981190b641f377b57fd009e83e6c
kokiyo1030 <kokiyo1030@naver.com>
2024-08-14 23:39:40 +0900

    filter

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -16,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -68,8 +69,33 @@ public class LoginController {
 //        return "redirect:/";
 //    }
 
+//    @PostMapping("/login")
+//    public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+//        if (bindingResult.hasErrors()) {
+//            return "login/loginForm";
+//        }
+//        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+//
+//        if (loginMember == null) {
+//            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
+//            return "login/loginForm";
+//        }
+//
+//        //로그인 성공 처리
+//        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+//        //기본값:true request.getSession(true);
+//        HttpSession session = request.getSession();
+//        //세션에 로그인 회원 정보 보관
+//        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+//        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+//
+//        return "redirect:/";
+//    }
+
     @PostMapping("/login")
-    public String loginV3(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+    public String loginV4(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/")String redirectURL,
+                          HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -88,7 +114,7 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
 
-        return "redirect:/";
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("/logout")
